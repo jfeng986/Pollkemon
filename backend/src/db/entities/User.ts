7e054c9c7d997bfe3846f47b8d6fd0880e42e4cf
jfeng986 <jfeng986@gmail.com>
2023-04-26 10:45:00 -0700

    first commit

---
+++ backend/src/db/entities/User.ts
@@ -0,0 +1,18 @@
+import { Entity, Property, Unique } from "@mikro-orm/core";
+import { BaseEntity } from "./BaseEntity.js";
+
+@Entity({ tableName: "users" })
+export class User extends BaseEntity {
+  @Property()
+  @Unique()
+  email!: string;
+
+  @Property()
+  name!: string;
+
+  @Property()
+  petType!: string;
+
+  @Property()
+  isMatched: boolean = false;
+}
